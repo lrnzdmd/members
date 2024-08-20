38f9c5ba8ef668e8795ae32447fb6d314d1465d6
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-08-20 21:29:35 +0200

    basic login logout signin template

---
+++ db/pool.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+require('dotenv').config()
+
+
+module.exports = new Pool({
+  host: process.env.DATABASE_HOST, 
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
---
+++ index.js
@@ -0,0 +1,107 @@
+const pool = require('./db/pool');
+const bcrypt = require('bcryptjs');
+const path = require('node:path')
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({extended: false}));
+
+passport.use(
+        new LocalStrategy(async (username, password, done) => {
+            try {
+                const { rows } = await pool.query("SELECT * FROM users WHERE username = $1;", [username]);
+                const user = rows[0];
+                if (!user) {
+                    return done(null, false, { message: 'incorrect username'});
+                }
+
+                const match = await bcrypt.compare(password, user.password);
+
+                if (!match) {
+                    return done(null,false, {message: 'incorrect password'});
+                }
+
+                return done (null, user);
+
+            } catch (error) {
+                return done(err);
+            }
+        })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id,done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE id = $1",[id]);
+        const user = rows[0];
+
+        done(null, user);
+
+    } catch (error) {
+        done(error);
+    }
+});
+
+app.use((req,res,next) => {
+    res.locals.currentUser = req.user;
+    next();
+})
+
+
+
+
+
+
+
+// ----------Views Render--------------
+
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/log-in', (req, res) => res.render('log-in'));
+
+app.post('/log-in', 
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/'
+    })
+);
+
+app.get('/log-out', (req,res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        } 
+        res.redirect('/');
+    });
+});
+
+app.get('/sign-up', (req, res) => res.render('sign-up'));
+
+app.post('/sign-up', (req, res, next) => {
+    try {
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return done(err);
+            }
+            await pool.query("INSERT INTO users (username, password) VALUES ($1,$2);",[req.body.username, hashedPassword]);
+            res.redirect('/');
+        })
+    } catch (error) {
+        return next(error);
+    }
+})
+
+
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
